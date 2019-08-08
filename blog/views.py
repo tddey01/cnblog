6689d310a28ce9b61266cb33112b223f0220a348
tddey <liujicun01@gmail.com>
2019-08-08 09:13:33 +0800

    博客系统之基于Ajax 提交 formdata 数据 的优化

--- blog/views.py
+++ blog/views.py
@@ -124,8 +124,8 @@ from django.forms import widgets
 
 
 class UserForm(forms.Form):
-    user = forms.CharField(max_length=32, label="用户名",widget=widgets.TextInput(attrs={'class':'form-control'}) )
-    pwd = forms.CharField(max_length=32, label="密码", widget=widgets.PasswordInput(attrs={'class':'form-control'}))
+    user = forms.CharField(max_length=32, label="用户名",widget=widgets.TextInput(attrs={'class':'form-control'}) , )
+    pwd = forms.CharField(max_length=32, label="密码", widget=widgets.PasswordInput(attrs={'class':'form-control'}),)
     re_pwd = forms.CharField(max_length=32, label="确认密码",  widget=widgets.PasswordInput(attrs={'class':'form-control'}))
     email = forms.EmailField(max_length=32, label="邮箱地址", widget=widgets.EmailInput(attrs={'class':'form-control'}))
 
